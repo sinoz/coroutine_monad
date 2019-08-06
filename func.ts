4c85f7e145160b72f1bfecc73c887a1b873e54c6
I.A <ilyas.alfan.b@gmail.com>
2019-08-06 18:59:46 +0200

    Fix function composition

--- func.ts
+++ func.ts
@@ -11,7 +11,7 @@ export let Func = <a, b>(f: (_: a) => b): Func<a, b> => {
     return {
       invoke: f,
       andThen: function<c>(g: Func<b, c>): Func<a, c> {
-        return Func<a, c>((x: a) => g.invoke(this.f(x)))
+        return Func<a, c>((x: a) => g.invoke(f(x)))
       }
     }
   }
