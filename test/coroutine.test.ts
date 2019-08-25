f62a3b083564265d18ffc752f14f0f9949a3e205
I.A <ilyas.alfan.b@gmail.com>
2019-08-25 18:01:12 +0200

    Add skeleton for `Coroutine` unit tests

---
+++ test/coroutine.test.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai"
+import { describe, it } from "mocha"
+
+describe('Coroutine constructors', function() {
+    it('succeed', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('suspend', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('fail', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('effect', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('transform', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('compute', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('fromOption', function() {
+      expect(3 * 5).equal(15);
+    });
+
+    it('fromEither', function() {
+      expect(3 * 5).equal(15);
+    });
+});
